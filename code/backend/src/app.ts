b5c80fba6cd025083d20c116bda5f55422902be3
Adriano Vieira <adriano.svieira@gmail.com>
2025-06-07 20:06:26 -0300

    Altera configuração listener
    - altera para que mostre a real configuração de endereço em uso

--- code/backend/src/app.ts
+++ code/backend/src/app.ts
@@ -3,9 +3,7 @@ import api from "./api";
 const API_HOST = process.env.API_HOST || "0.0.0.0";
 const API_PORT = process.env.API_PORT || 8000;
 
-api.set("port", API_PORT);
-api.set("host", API_HOST);
-
-api.listen(() => {
-  console.log(`API is running on ${API_PORT} port.`);
+const server = api.listen(API_PORT as number, API_HOST as string, (result) => {
+  const address = server.address();
+  console.log("API em excução em %s", address);
 });
