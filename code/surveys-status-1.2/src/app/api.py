0cfb3ae6b3d39b9104b79fb0dd05c310bf4e1996
Adriano Vieira <adriano.svieira@gmail.com>
2025-06-16 03:25:13 -0300

    Aprimora módulo api
    - viabiliza execução em modo debug

--- code/surveys-status-1.2/src/app/api.py
+++ code/surveys-status-1.2/src/app/api.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from routes import health
+import uvicorn
+from routes import health, surveys
 
 app = FastAPI()
 
 app.include_router(health.router)
+app.include_router(surveys.router)
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="0.0.0.0", reload=True)
