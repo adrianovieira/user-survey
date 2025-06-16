2219173a60e7aa74f45482cc0c10fc537412d2db
Adriano Vieira <adriano.svieira@gmail.com>
2025-06-16 17:12:35 -0300

    Adiciona casos de testers para busca de pesquisas
    - adiciona casos de testes para busca com e sem filtros

--- code/surveys-status-1.2/src/tests/unit/conftest.py
+++ code/surveys-status-1.2/src/tests/unit/conftest.py
@@ -6,3 +6,15 @@ from app.api import app
 @pytest.fixture
 def client():
     yield TestClient(app)
+
+
+@pytest.fixture
+def surveys_status_filter():
+    return {
+        "createdAt": {
+            "start": "2025-01-01T00:19:30.899Z",
+            "end": "2025-12-31T23:19:30.899Z",
+        },
+        "limit": 100,
+        "offset": 10,
+    }
---
+++ code/surveys-status-1.2/src/tests/unit/test_surveys_status.py
@@ -0,0 +1,65 @@
+class TestSurveys:
+
+    def test_surveys_status(self, client):
+        response = client.post("/surveys/status")
+
+        assert response.status_code == 200
+        content = response.json()
+        content_one: dict = content[0]
+
+        assert "date" in content_one.keys()
+        assert "total" in content_one.keys()
+        assert "status" in content_one.keys()
+        assert any(content_one["status"].values())
+
+    def test_surveys_status_filter(self, client, surveys_status_filter: dict):
+
+        response = client.post("/surveys/status", json=surveys_status_filter)
+        assert response.status_code == 200
+        content = response.json()
+        content_one: dict = content[0]
+        assert "date" in content_one.keys()
+        assert "total" in content_one.keys()
+        assert "status" in content_one.keys()
+        assert any(content_one["status"].values())
+
+        surveys_status_filter.pop("offset")
+        response = client.post("/surveys/status", json=surveys_status_filter)
+        assert response.status_code == 200
+        content = response.json()
+        content_one: dict = content[0]
+        assert "date" in content_one.keys()
+        assert "total" in content_one.keys()
+        assert "status" in content_one.keys()
+        assert any(content_one["status"].values())
+
+        surveys_status_filter.pop("limit")
+        response = client.post("/surveys/status", json=surveys_status_filter)
+        assert response.status_code == 200
+        content = response.json()
+        content_one: dict = content[0]
+        assert "date" in content_one.keys()
+        assert "total" in content_one.keys()
+        assert "status" in content_one.keys()
+        assert any(content_one["status"].values())
+
+        surveys_status_filter["createdAt"].pop("start")
+        response = client.post("/surveys/status", json=surveys_status_filter)
+        assert response.status_code == 200
+        content = response.json()
+        content_one: dict = content[0]
+        assert "date" in content_one.keys()
+        assert "total" in content_one.keys()
+        assert "status" in content_one.keys()
+        assert any(content_one["status"].values())
+
+        surveys_status_filter["createdAt"].pop("end")
+        response = client.post("/surveys/status", json=surveys_status_filter)
+
+        assert response.status_code == 200
+        content = response.json()
+        content_one: dict = content[0]
+        assert "date" in content_one.keys()
+        assert "total" in content_one.keys()
+        assert "status" in content_one.keys()
+        assert any(content_one["status"].values())
