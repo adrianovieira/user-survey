4f51c4092c67ebbfeb9ae29d2619ef1913413d39
Adriano Vieira <adriano.svieira@gmail.com>
2025-06-22 02:17:08 -0300

    Aprimora api com tratamento de erros
    - adiciona exception handlers à instância da app

--- code/surveys-status-1.2/src/app/api.py
+++ code/surveys-status-1.2/src/app/api.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+
 from routes import health, surveys
+from service.handlers.exceptions import APIExceptionError, api_exception_handler
+
+from pydantic import ValidationError
 
 app = FastAPI()
+
+app.add_exception_handler(APIExceptionError, api_exception_handler)
+app.add_exception_handler(ValidationError, api_exception_handler)
+app.add_exception_handler(RequestValidationError, api_exception_handler)
 
 app.include_router(health.router)
 app.include_router(surveys.router)
