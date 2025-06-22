03d9b921c4c5e6b527ef75e8b3e78bbb57f612e7
Adriano Vieira <adriano.svieira@gmail.com>
2025-06-22 20:38:29 -0300

    Altera exception handler da app
    - altera para exception genérica APIException
    - reorganiza lista de módulos importados

--- code/surveys-status-1.2/src/app/api.py
+++ code/surveys-status-1.2/src/app/api.py
@@ -1,15 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
-
-from routes import health, surveys
-from service.handlers.exceptions import APIExceptionError, api_exception_handler
-
 from pydantic import ValidationError
+from routes import health, surveys
+from service.handlers.exceptions import APIException, api_exception_handler
 
 app = FastAPI()
 
-app.add_exception_handler(APIExceptionError, api_exception_handler)
+app.add_exception_handler(APIException, api_exception_handler)
 app.add_exception_handler(ValidationError, api_exception_handler)
 app.add_exception_handler(RequestValidationError, api_exception_handler)
 
