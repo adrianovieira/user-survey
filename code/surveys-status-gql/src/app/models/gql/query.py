412c9913643cf8ba9bac68437ed79ee617929f15
Adriano Vieira <adriano.svieira@gmail.com>
2025-07-01 15:38:25 -0300

    Remove query users
    - remove por ser ad-hoc ;)

--- code/surveys-status-gql/src/app/models/gql/query.py
+++ code/surveys-status-gql/src/app/models/gql/query.py
@@ -1,7 +1,5 @@
 from app.controllers.gql.surveys_query import query_surveys_status
-from app.controllers.gql.users_query import query_users
 from app.models.gql.surveys import SurveysStatus
-from app.models.gql.users import User
 from strawberry import Schema
 from strawberry import field as strawberry_field
 from strawberry import type as strawberry_type
@@ -9,7 +7,6 @@ from strawberry import type as strawberry_type
 
 @strawberry_type
 class Query:
-    users: list[User] = strawberry_field(resolver=query_users)
     surveysStatus: list[SurveysStatus] = strawberry_field(resolver=query_surveys_status)
 
 
